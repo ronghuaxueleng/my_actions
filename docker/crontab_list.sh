17cb04123929ec8411f80245c0809379ed381867
caoqiang <Xinyan1203>
2021-04-12 11:13:03 +0800

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,2 +1,8 @@
 #拾取好友与自己零件、厂长翻倍任务、点击厂长任务
 0 1,18 * * * node /scripts/jd_factory_component.js >> /scripts/logs/jd_factory_component.log 2>&1
+# 第一次执行,大概率失败 {23：59}
+59 59 23 * * * node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
+# 第二次执行,可以执行完毕 {00:02}
+30 2 0 * * * node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
+# 1,2 0 0 * * *
+1,2 0 0 * * * node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
