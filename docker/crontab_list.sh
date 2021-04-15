5d46383bffdadba57a0616e8e0fbd43fff1df4e1
caoqiang <Xinyan1203>
2021-04-15 20:15:53 +0800

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,2 +1,3 @@
 #拾取好友与自己零件、厂长翻倍任务、点击厂长任务
 0 1,18 * * * node /scripts/jd_factory_component.js >> /scripts/logs/jd_factory_component.log 2>&1
+10 10 * * * node /scripts/jd_xmf.js >> /scripts/logs/jd_xmf.log 2>&1
