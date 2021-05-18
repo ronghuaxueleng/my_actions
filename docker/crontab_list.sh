128288dd2fa5056f42026d8e33600a30997954a7
caoqiang <Xinyan1203>
2021-05-18 18:23:46 +0800

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -2,3 +2,5 @@
 0 1,18 * * * node /scripts/jd_factory_component.js >> /scripts/logs/jd_factory_component.log 2>&1
 10 10 * * * node /scripts/jd_xmf.js >> /scripts/logs/jd_xmf.log 2>&1
 0 0 * * * node /scripts/jx_cfdtx.js >> /scripts/logs/jx_cfdtx.log 2>&1
+#joy总动员
+30 10 * * * node /scripts/jd_friend.js >> /scripts/logs/jd_friend.log 2>&1
