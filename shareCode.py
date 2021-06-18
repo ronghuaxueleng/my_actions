6f254918219537de2f95cb00dc0dab9aa65622c9
ronghuaxueleng <caoqianghappy@126.com>
2021-06-18 14:34:08 +0000

    up

--- shareCode.py
+++ shareCode.py
@@ -10,11 +10,15 @@ password = os.getenv('password')
 api_id = os.getenv('api_id')
 api_hash = os.getenv('api_hash')
 
+print(phone)
+
 host = '127.0.0.1'
 port = 7891
 proxy = (socks.SOCKS5, host, port)
 
-client = TelegramClient("sharecode", api_id, api_hash, proxy=proxy).start(phone=phone, password=password)
+sessionFilePath = os.getenv('sessionFilePath') if os.getenv('sessionFilePath') else ''
+
+client = TelegramClient(sessionFilePath + "/sharecode/" + phone, api_id, api_hash, proxy=proxy).start(phone=phone, password=password)
 
 async def main():
     await client.get_me()
