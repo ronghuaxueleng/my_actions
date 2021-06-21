dcb8de24ba810bbef70dc27492b7412b83cc99cb
caoqiang <Xinyan1203>
2021-06-21 09:53:32 +0800

    up

--- git_pull.sh
+++ git_pull.sh
@@ -36,7 +36,7 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ScriptsURL=https://hub.fastgit.org/chinnkarahoi/jd_scripts
+ScriptsURL=https://hub.fastgit.org/JDHelloWorld/jd_scripts
 Scripts3URL=https://gitee.com/getready/my_actions.git
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
