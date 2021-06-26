e2d63a163fc1214e1fff621fbed5f79e10fdb6dc
caoqiang <Xinyan1203>
2021-06-26 21:07:05 +0800

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -17,6 +17,6 @@
 #生成京喜农场互助码
 59 23 * * * node /scripts/get_jxnc_token.js >> /scripts/logs/get_jxnc_token.log 2>&1
 #获取助力码
-58 20-22 * * 6 node /scripts/jd_get_share_code_4_bot.js >> /scripts/logs/jd_get_share_code_4_bot.log 2>&1
+58 20 * * 6 node /scripts/jd_get_share_code_4_bot.js >> /scripts/logs/jd_get_share_code_4_bot.log 2>&1
 # 提交助力码
-0,1,2,3,4,5,15,30 21-23 * * 6 bash -x /jd/shareCode.sh >> /jd/log/shareCode.log 2>&1
+0,1,2 21 * * 6 bash -x /jd/shareCode.sh >> /jd/log/shareCode.log 2>&1
