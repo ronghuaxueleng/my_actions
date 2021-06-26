c63b517b31fadf1145cf65818bb046e89d16cf55
caoqiang <Xinyan1203>
2021-06-27 06:44:13 +0800

    up

--- shareCode.sh
+++ shareCode.sh
@@ -10,6 +10,4 @@ for ((i = 0; i < ${#phones[@]}; i++)); do
     /usr/bin/python3 /jd/shareCode.py ${phones[i]} ${passwords[i]} ${api_ids[i]} ${api_hashs[i]} ${sharedCodesJsonPaths[i]}
 done
 
-for ((i = 0; i < ${#phones[@]}; i++)); do
-    /usr/bin/python3 /jd/verificationShareCodeUrl.py ${sharedCodesJsonPaths[i]}
-done
+/usr/bin/python3 /jd/verificationShareCodeUrl.py ${sharedCodesJsonPaths[0]}
