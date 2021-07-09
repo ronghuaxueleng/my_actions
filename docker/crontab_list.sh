0c0e0a4f901a73ba26aa7a4c4c7db3cf3868bb75
ronghuaxueleng <caoqianghappy@126.com>
2021-07-09 08:49:05 +0000

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -356,6 +356,8 @@
 10 * * * * node /scripts/jd_daily_egg.js >> /scripts/logs/jd_daily_egg.log 2>&1
 #京东京喜工厂 
 10 * * * * node /scripts/jd_dreamFactory.js >> /scripts/logs/jd_dreamFactory.log 2>&1
+#京东东东农场 
+5 6-18/6 * * * node /scripts/jd_fruit.js >> /scripts/logs/jd_fruit.log 2>&1
 #京东获取互助码 
 20 13 * * 6 node /scripts/jd_get_share_code.js >> /scripts/logs/jd_get_share_code.log 2>&1
 #京东东东工厂 
