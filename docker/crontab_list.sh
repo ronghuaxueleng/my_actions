372fa0f3fd9b7d64a8f0d74f378f558aa5943e24
ronghuaxueleng <caoqianghappy@126.com>
2021-07-09 18:57:14 +0000

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -42,7 +42,7 @@
 #全民抢京豆
 1,10,20 0 * * * node /scripts/jd_qjd.js >> /scripts/logs/jd_qjd.log 2>&1
 #QQ星
-20 6,12,18 * * * node /scripts/jd_qqxing.js >> /scripts/logs/jd_qqxing.log 2>&1
+1 0-23/2 * * * bash jd jd_qqxing
 #众筹许愿池
 20 12 * * * node /scripts/jd_wishingPool.js >> /scripts/logs/jd_wishingPool.log 2>&1
 #汪汪乐园
@@ -278,7 +278,6 @@
 #京东众筹许愿池柠檬版 
 0 8,12 * * * node /scripts/jd_wish.js >> /scripts/logs/jd_wish.log 2>&1
 #京东汪汪乐园柠檬版 
-0 */1 * * * node /scripts/jd_joy-park.js >> /scripts/logs/jd_joy-park.log 2>&1
 #京东京享值PK史蒂夫版 
 15 0,6,13,19,21 * * node /scripts/jxzpk.js >> /scripts/logs/jxzpk.log 2>&1
 #京东京享值PK史蒂夫版 
@@ -486,7 +485,6 @@
 #京喜工厂商品列表详情 
 10 10 * * * node /scripts/jx_products_detail.js >> /scripts/logs/jx_products_detail.log 2>&1
 #京东赚京东开团 
-10 * * * * node /scripts/jd_zjd_tuan.js >> /scripts/logs/jd_zjd_tuan.log 2>&1
 #京东读书签到 
 0 10 0 * * * node /scripts/jddsSign.js >> /scripts/logs/jddsSign.log 2>&1
 #京东 
