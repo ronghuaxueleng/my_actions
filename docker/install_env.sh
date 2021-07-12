63038eb9a988bcf7e4c78b8b401c8b2f4c5f99a3
ronghuaxueleng <caoqianghappy@126.com>
2021-07-12 11:36:42 +0000

    up

--- docker/install_env.sh
+++ docker/install_env.sh
@@ -15,6 +15,6 @@ cd ${ScriptsDir}
 pip3 install requests
 npm install -g npm npm-install-peers
 npm install -g ts-node typescript --unsafe-perm=true --allow-root
-npm install -g ts-node typescript @types/node date-fns axios
+npm install -g ts-node typescript @types/node date-fns axios require tslib fs
 npm install --save-dev @types/node
 echo -e '\n\033[32m ------- 忽略 WARN 警告类输出内容 -------\033[0m'
