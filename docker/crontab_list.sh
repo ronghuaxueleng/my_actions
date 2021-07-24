f5fa2e1e411d4673f603eb1cfcd2234a443d3793
caoqiang <Xinyan1203>
2021-07-24 11:08:22 +0800

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -7,7 +7,7 @@
 # 京豆变更通知
 5 9 * * * bash jd jd_bean_change
 # 更新代理配置
-* 0 * * * cd /root/.config/clash && /usr/bin/bash refresh.sh
+* 3 * * * cd /root/.config/clash && /usr/bin/bash refresh.sh
 #拾取好友与自己零件、厂长翻倍任务、点击厂长任务
 0 1,18 * * * node /scripts/jd_factory_component.js >> /scripts/logs/jd_factory_component.log 2>&1
 #财富岛提现
@@ -19,9 +19,9 @@
 #获取助力码
 58 20 * * 6 node /scripts/jd_get_share_code_4_bot.js >> /scripts/logs/jd_get_share_code_4_bot.log 2>&1
 # 提交助力码
-0,1,2 21 * * 6 bash -x /jd/shareCode.sh >> /jd/log/shareCode.log 2>&1
+0 21 * * 6 bash -x /jd/shareCode.sh >> /jd/log/shareCode.log 2>&1
 # 财富岛热气球
-0 * * * * node /scripts/jd_cfd_loop.js >> /scripts/logs/jd_cfd_loop.log 2>&1
+1 * * * * node /scripts/jd_cfd_loop.js >> /scripts/logs/jd_cfd_loop.log 2>&1
 # 预存验证
 50 * * * * node /scripts/jd_validate_Worker.js >> /scripts/logs/jd_validate_Worker.log 2>&1
 # 零食街
