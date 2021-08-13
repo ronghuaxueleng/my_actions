e695b936d5f026e37323c91b7e04d0819c123c3e
ronghuaxueleng <caoqianghappy@126.com>
2021-08-13 17:08:08 +0000

    up

--- jd_decompression.js
+++ jd_decompression.js
@@ -1,7 +1,7 @@
 /**
-蚊子腿豆子，24号应该可以参与瓜分
-活动到24号。一天可以跑2次
-cron 5 6,8 12-24 8 *  https://raw.githubusercontent.com/star261/jd/main/scripts/jd_appliances.js
+ * 蚊子腿豆子，24号应该可以参与瓜分
+ * 活动到24号。一天可以跑2次
+ cron "25 5,11 12-24 8 *"  jd_decompression.js
  */
 const $ = new Env('热血心跳,狂解压');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
@@ -9,7 +9,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [];
 $.activityID = 'dz2107100008586804';
 $.shopid = '1000085868';
-const inCode= ['4efc89e0a5604304bd9414c21312ab0d','bc6d0a6db4ee45c3bd5d1730c967224f'];
+const inCode= ['0ae7d1b2e4384ca7878c0b5f7e424a44'];
 $.shareUuid = getRandomArrayElements(inCode,1)[0];
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
