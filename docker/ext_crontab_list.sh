8d11e243a6a75bb6b1bce6f306f8a9ed27859ecd
ronghuaxueleng <caoqianghappy@126.com>
2021-08-29 03:06:43 +0000

    up

---
+++ docker/ext_crontab_list.sh
@@ -0,0 +1,6 @@
+# 开卡
+0 8,15 * * * cd /jd/OpenCard && bash run.sh
+# 更新代理配置
+* 3 * * * cd /root/.config/clash && /bin/bash refresh.sh
+# 提交助力码
+0 21 * * 6 bash -x /jd/shareCode.sh >> /jd/log/shareCode.log 2>&1
