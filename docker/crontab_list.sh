a0beb8d0ad1e413a35969c1cb50e954fa1f1ea7b
ronghuaxueleng <caoqianghappy@126.com>
2021-08-30 11:16:43 +0800

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -5,9 +5,9 @@
 #拾取好友与自己零件、厂长翻倍任务、点击厂长任务
 0 1,18 * * * node /scripts/jd_factory_component.js >> /scripts/logs/jd_factory_component.log 2>&1
 #财富岛提现
-0 0,12 * * * node /scripts/jd_cfdtx.js >> /scripts/logs/jd_cfdtx.log 2>&1
+# 0 0,12 * * * node /scripts/jd_cfdtx.js >> /scripts/logs/jd_cfdtx.log 2>&1
 #生成财富岛提现token
-59 23 * * * node /scripts/get_jx_token.js >> /scripts/logs/get_jx_token.log 2>&1
+# 59 23 * * * node /scripts/get_jx_token.js >> /scripts/logs/get_jx_token.log 2>&1
 #生成京喜农场互助码
 59 23 * * * node /scripts/get_jxnc_token.js >> /scripts/logs/get_jxnc_token.log 2>&1
 #获取助力码
