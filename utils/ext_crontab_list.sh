faa7246799f90124f840afcb22f7d25f9c436077
ronghuaxueleng <caoqianghappy@126.com>
2021-09-04 20:34:24 +0800

    up

--- utils/ext_crontab_list.sh
+++ utils/ext_crontab_list.sh
@@ -6,3 +6,5 @@
 0 21 * * 6 bash -x /jd/shareCode.sh >> /jd/log/shareCode.log 2>&1
 # 提交助力码
 0 1 * * * bash /jd/submitme.sh >> /jd/log/submitme.log 2>&1
+# 更新cookie
+0 12,23 * * * python updateCookie.py
