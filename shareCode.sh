9c077c3b1454cb49d8a3f21df6e9f73f4a9f2a34
ronghuaxueleng <caoqianghappy@126.com>
2021-09-13 15:09:02 +0800

    up

--- shareCode.sh
+++ shareCode.sh
@@ -10,4 +10,4 @@ for ((i = 0; i < ${#phones[@]}; i++)); do
     /usr/bin/python3 /jd/shareCode.py ${phones[i]} ${passwords[i]} ${api_ids[i]} ${api_hashs[i]} ${sharedCodesJsonPaths[i]}
 done
 
-/usr/bin/python3 /jd/verificationShareCodeUrl.py ${sharedCodesJsonPaths[0]}
+# /usr/bin/python3 /jd/verificationShareCodeUrl.py ${sharedCodesJsonPaths[0]}
