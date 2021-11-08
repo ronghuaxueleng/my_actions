8e0dfe304425a12dab17f6065d2b2b1fdf780d04
ronghuaxueleng <caoqianghappy@126.com>
2021-11-08 13:58:52 +0800

    up

--- docker/refresh.sh
+++ docker/refresh.sh
@@ -4,4 +4,5 @@ wget -O config.yaml https://gitee.com/getready/get_v2/raw/main/c7
 # 有些供应商会把 external-controller 设为 127.0.0.1，导致很多人搭起来连不上。这里替换成 0.0.0.0
 sed -i 's/127.0.0.1:9090/0.0.0.0:9090/g' config.yaml
 # 重启 clash
+ps -ef | grep clash | grep -v grep | awk '{print $2}' | xargs kill -9
 clash
