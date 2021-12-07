486a5ddd2e2f815373cf00cac6c6764199748b7e
ronghuaxueleng <caoqianghappy@126.com>
2021-12-07 06:21:56 +0000

    up

--- jd_redrain.js
+++ jd_redrain.js
@@ -85,13 +85,13 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
         message = '';
         await TotalBean();
         console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
-        if (!$.isLogin) {
-          $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/`, {"open-url": "https://bean.m.jd.com/"});
-          if ($.isNode()) {
-            await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
-          }
-          continue
-        }
+        //if (!$.isLogin) {
+        //  $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/`, {"open-url": "https://bean.m.jd.com/"});
+        //  if ($.isNode()) {
+        //    await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
+        //  }
+        //  continue
+        //}
         let nowTs = new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000
         // console.log(nowTs, $.startTime, $.endTime)
         // await showMsg();
