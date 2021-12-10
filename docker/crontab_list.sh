d116db31fab8a54a6bdf47de627bcb6ee92d32e2
ronghuaxueleng <caoqianghappy@126.com>
2021-12-10 04:23:52 +0000

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -463,7 +463,7 @@
 # 金榜年终奖
 5 0,2 * * * node /scripts/jd_split.js >> /scripts/logs/jd_split.log 2>&1
 # 京享红包
-0 0,12,18,20 * * * node /scripts/jd_redEnvelope.js >> /scripts/logs/jd_redEnvelope.log 2>&1
+0 0,12 * * * node /scripts/jd_redEnvelope.js >> /scripts/logs/jd_redEnvelope.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
