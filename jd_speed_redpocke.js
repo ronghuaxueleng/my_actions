6fea861e32b42b213df8af3abd3eb7febde0fa57
ronghuaxueleng <caoqianghappy@126.com>
2021-12-27 09:37:46 +0000

    up

--- jd_speed_redpocke.js
+++ jd_speed_redpocke.js
@@ -396,7 +396,8 @@ function cashOut(id,poolBaseId,prizeGroupId,prizeBaseId,) {
 function invite2() {
   let inviterIdArr = [
     "5V7vHE23qh2EkdBHXRFDuA==",
-    "wXX9SjXOdYMWe5Ru/1+x9A=="
+    "Yvk/fMWJC/6lvcx1iUFnsw==",
+    "E9EvSFNuA1pahSQT0uSsXkW1v0j+QOHQbk8+peJYc0I="
   ]
   let inviterId = inviterIdArr[Math.floor((Math.random() * inviterIdArr.length))]
   let options = {
