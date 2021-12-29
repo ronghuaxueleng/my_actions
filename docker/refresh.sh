6a1a9c3093a9ed071caf515baa8b52e0644f3608
ronghuaxueleng <caoqianghappy@126.com>
2021-12-29 15:16:12 +0800

    up

--- docker/refresh.sh
+++ docker/refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 下载节点配置文件
-wget -O config.yaml https://gitee.com/getready/get_v2/raw/main/c7
+wget -O config.yaml https://gitee.com/getready/get_v2/raw/main/pub/bhqz.yaml
 # 有些供应商会把 external-controller 设为 127.0.0.1，导致很多人搭起来连不上。这里替换成 0.0.0.0
 sed -i 's/127.0.0.1:9090/0.0.0.0:9090/g' config.yaml
 # 重启 clash
