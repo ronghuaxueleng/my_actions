c431c04cf0050088e28b40ff829c9f2764008771
ronghuaxueleng <caoqianghappy@126.com>
2022-03-01 09:41:01 +0000

    up

--- utils/ext_crontab_list.sh
+++ utils/ext_crontab_list.sh
@@ -1,13 +1,13 @@
 # 开卡
 0 8,15 * * * cd /jd/OpenCard && bash run.sh
 # 更新代理配置
-* 3 * * * cd /root/.config/clash && /bin/bash refresh.sh
+# * 3 * * * cd /root/.config/clash && /bin/bash refresh.sh
 # 向机器人提交助力码
-0 9,21 * * * bash -x /jd/shareCode.sh >> /jd/log/shareCode.log 2>&1
-0 0 * * 1 bash -x /jd/shareCode1.sh >> /jd/log/shareCode1.log 2>&1
-0 8,9 * * * bash -x /jd/shareCode2.sh >> /jd/log/shareCode2.log 2>&1
+# 0 9,21 * * * bash -x /jd/shareCode.sh >> /jd/log/shareCode.log 2>&1
+# 0 0 * * 1 bash -x /jd/shareCode1.sh >> /jd/log/shareCode1.log 2>&1
+# 0 8,9 * * * bash -x /jd/shareCode2.sh >> /jd/log/shareCode2.log 2>&1
 # 提交助力码
-0 1 * * * bash /jd/submitme.sh >> /jd/log/submitme.log 2>&1
+# 0 1 * * * bash /jd/submitme.sh >> /jd/log/submitme.log 2>&1
 # 更新cookie
 #2 7 * * * python updateConfig.py /jd/config/config.sh >> /jd/log/updateCookie.py.log 2>&1
 #获取助力码
