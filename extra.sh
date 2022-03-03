bafb1639e496b9b19644c5d0e448dc54c2ff6a52
ronghuaxueleng <caoqianghappy@126.com>
2022-03-03 03:44:10 +0000

    up

--- extra.sh
+++ extra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Update: 2022-02-25
-# Content: del  jd_xinruimz.js jd_health_plant.py
+# Update: 2022-03-03
+# Content: add  jd_plusReward.js(逛plus，抽京豆)
 
 ##############################  京  东  商  城  ##############################
 ## 列表格式： 脚本名称 | 活动名称 | 备注说明
@@ -36,6 +36,7 @@
 #  jd_EsportsManager.js            东东电竞经理
 #  jd_wyw.js                       玩一玩成就
 #  jd_sxLottery.js                 京东生鲜每日抽奖
+#  jd_plusReward.js                逛plus，抽京豆
 
 ##############################  脚  本  内  环  境  变  量  ##############################
 ## 推荐使用项目自带的环境变量管理命令，默认交互支持快捷命令
@@ -65,17 +66,6 @@
 
 ##############################  近  期  删  除  ##############################
 
-# jd_ms.js
-# jd_xmf.js
-# jd_bt_sign.js
-# jx_ttysq.js
-# jd_mpdzcar.js
-# jd_mpdzcar_game.js
-# jd_mpdzcar_help.js
-# jd_angryKoi.py
-# jd_xinruimz.js
-# jd_health_plant.py
-
 ##############################  京  东  到  家  ##############################
 
 ## jddj_bean.js                     鲜豆任务
@@ -93,8 +83,8 @@
 ## 免责声明：当作者的脚本不可拉取时，会临时启用别人 FORK 或搬运的库代替
 
 NEWLINE="\n          "
-UpdateDate="2022-02-25"
-UpdateContent="del  jd_xinruimz.js jd_health_plant.py"
+UpdateDate="2022-03-03"
+UpdateContent="add  jd_plusReward.js(逛plus，抽京豆)"
 
 ## 作者
 author_list="Public passerby_b smiek2221 star261 yyds X1a0He KingRan Dellear jiulan"
@@ -321,7 +311,7 @@ for del in ${DeleteCacheFiles}; do
 done
 
 ## 删除脚本和定时
-DeleteScripts="jd_ms.js jd_xmf.js jd_bt_sign.js jx_ttysq.js jd_mpdzcar.js jd_mpdzcar_game.js jd_mpdzcar_help.js jd_angryKoi.py jd_xinruimz.js jd_health_plant.py"
+DeleteScripts=""
 for del in ${DeleteScripts}; do
   [ -f $ScriptsDir/$del ] && rm -rf $ScriptsDir/$del && sed -i "/ $TaskCmd $(echo "$del" | awk -F\. '{print $1}' | perl -pe "{s|^jd_||; s|^jx_||; s|^jr_||;}")/d" $ListCrontabUser
 done
