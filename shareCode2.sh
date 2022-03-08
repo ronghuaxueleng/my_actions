8f929f1a713b2db7bfa38d397b8408ce3f75b18c
ronghuaxueleng <caoqianghappy@126.com>
2022-03-08 10:38:59 +0800

    up

--- shareCode2.sh
+++ shareCode2.sh
@@ -7,7 +7,7 @@ api_hashs=(${api_hash//@/ })
 sharedCodesJsonPaths=(${sharedCodesJsonPath//@/ })
 
 for ((i = 0; i < ${#phones[@]}; i++)); do
-    /usr/local/bin/python3 /jd/shareCode2.py ${phones[i]} ${passwords[i]} ${api_ids[i]} ${api_hashs[i]} ${sharedCodesJsonPaths[i]}
+    /usr/local/bin/python3 /jd/shareCode2.py ${phones[i]} ${passwords[i]} ${api_ids[i]} ${api_hashs[i]} ${sharedCodesJsonPaths[i]} '@passerbybbot' 'true'
 done
 
 # /usr/local/bin/python3 /jd/verificationShareCodeUrl.py ${sharedCodesJsonPaths[0]}
