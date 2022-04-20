971171143ad8497910f86fa27311b21003e5e28d
ronghuaxueleng <caoqianghappy@126.com>
2022-04-20 18:47:24 +0000

    up

--- extra.sh
+++ extra.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Update: 2022-04-19
-# Content: del  jd_april.js jd_april2.js
+# Update: 2022-04-21
+# Content: add  jd_desire.js(集魔方)
+#          del  jd_carnivalcity.js
 
 ##############################  京  东  商  城  ##############################
 ## 列表格式： 脚本名称 | 活动名称 | 备注说明
@@ -37,7 +38,7 @@
 #  jd_mpdzcar_help.js              京东汽车头文字J助力
 #  jd_ddly.js                      东东乐园
 #  jd_speed_redpocke.js            京东极速版红包
-#  jd_carnivalcity.js              京东手机狂欢城
+#  jd_desire.js                    集魔方
 
 ##############################  脚  本  内  环  境  变  量  ##############################
 ## 推荐使用项目自带的环境变量管理命令，默认交互支持快捷命令
@@ -67,14 +68,14 @@
 
 ##############################  近  期  删  除  ##############################
 
-# jd_april.js 
-# jd_april2.js
+# jd_carnivalcity.js
+
 ##############################  主  要  代  码  ##############################
 ## 免责声明：当作者的脚本不可拉取时，会临时启用别人 FORK 或搬运的库代替
 
 NEWLINE="\n          "
-UpdateDate="2022-04-19"
-UpdateContent="del  jd_april.js jd_april2.js"
+UpdateDate="2022-04-21"
+UpdateContent="add  jd_desire.js(集魔方)${NEWLINE}del  jd_carnivalcity.js"
 
 ## 作者
 author_list="Public smiek2221 star261 yyds X1a0He KingRan Dellear jiulan"
@@ -110,7 +111,7 @@ my_scripts_list_star261="jd_productZ4Brand.js jd_fan.js"
 
 ## yyds
 scripts_base_url_yyds=https://raw.githubusercontent.com/okyyds/yydspure/master/
-my_scripts_list_yyds="JDSignValidator.js jd_try.js jd_try_notify.py jd_morningSc.js jd_moneyTree_heip.js jd_dwapp.js jd_unsubscriLive.js jd_nnfls.js jd_gold_sign.js jd_beauty_ex.js jd_wq_wxsign.js jd_wyw.js jd_mpdzcar.js jd_mpdzcar_game.js jd_mpdzcar_help.js jd_ddly.js jd_speed_redpocke.js jd_carnivalcity.js"
+my_scripts_list_yyds="JDSignValidator.js jd_try.js jd_try_notify.py jd_morningSc.js jd_moneyTree_heip.js jd_dwapp.js jd_unsubscriLive.js jd_nnfls.js jd_gold_sign.js jd_beauty_ex.js jd_wq_wxsign.js jd_wyw.js jd_mpdzcar.js jd_mpdzcar_game.js jd_mpdzcar_help.js jd_ddly.js jd_speed_redpocke.js"
 
 ## X1a0He
 scripts_base_url_X1a0He=https://raw.githubusercontent.com/X1a0He/jd_scripts_fixed/main/
@@ -118,7 +119,7 @@ my_scripts_list_X1a0He="jd_unsubscribe_xh.js"
 
 ## KingRan
 scripts_base_url_KingRan=https://raw.githubusercontent.com/KingRan/KR/main/
-my_scripts_list_KingRan="jd_wish.js jd_joy_park.js jd_joy_park_task.js jd_m_sign.js"
+my_scripts_list_KingRan="jd_wish.js jd_joy_park.js jd_joy_park_task.js jd_m_sign.js jd_desire.js"
 
 ## Dellear
 scripts_base_url_Dellear=https://raw.githubusercontent.com/Dellear/lost/main/extra/
@@ -297,7 +298,7 @@ for del in ${DeleteCacheFiles}; do
 done
 
 ## 删除脚本和定时
-DeleteScripts="jd_april.js jd_april2.js"
+DeleteScripts="jd_carnivalcity.js"
 for del in ${DeleteScripts}; do
   [ -f $ScriptsDir/$del ] && rm -rf $ScriptsDir/$del && sed -i "/ $TaskCmd $(echo "$del" | awk -F\. '{print $1}' | perl -pe "{s|^jd_||; s|^jx_||; s|^jr_||;}")/d" $ListCrontabUser
 done
