2d73733b2057bfc948a2a5d3c3f50c51f7466bf3
ronghuaxueleng <caoqianghappy@126.com>
2022-06-05 03:13:19 +0000

    up

--- jdFruitShareCodes.js
+++ jdFruitShareCodes.js
@@ -6,7 +6,10 @@
 //云服务器腾讯云函数等NOde.js用户在此处填写京东东农场的好友码。
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
-let FruitShareCodes = []
+let FruitShareCodes = [
+  '',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '',//账号二的好友shareCode，不同好友中间用@符号隔开
+]
 
 // 从日志获取互助码
 // const logShareCodes = require('./utils/jdShareCodes');
