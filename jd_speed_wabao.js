d8aa683d2f402951c49e69215322c4b83eacd5f2
ronghuaxueleng <caoqianghappy@126.com>
2022-09-14 16:30:25 +0000

    up

--- jd_speed_wabao.js
+++ jd_speed_wabao.js
@@ -146,80 +146,79 @@ var Jd_speed_wabao = /** @class */ (function (_super) {
     };
     Jd_speed_wabao.prototype.main = function (user) {
         return __awaiter(this, void 0, void 0, function () {
-            var _a, _b, res, data, _c, _d, t, e_1_1, e_2;
-            var e_1, _e;
-            return __generator(this, function (_f) {
-                switch (_f.label) {
+            var fp, _a, res, data, _b, _c, t, e_1_1, e_2;
+            var e_1, _d;
+            return __generator(this, function (_e) {
+                switch (_e.label) {
                     case 0:
-                        _a = this;
-                        _b = process.env.FP_8DD95;
-                        if (_b) return [3 /*break*/, 2];
+                        _a = process.env.FP_8DD95;
+                        if (_a) return [3 /*break*/, 2];
                         return [4 /*yield*/, this.getFp()];
                     case 1:
-                        _b = (_f.sent());
-                        _f.label = 2;
+                        _a = (_e.sent());
+                        _e.label = 2;
                     case 2:
-                        _a.fp = _b;
+                        fp = _a;
                         this.user = user;
                         this.user.UserAgent = "jdltapp;iPhone;3.9.2;Mozilla/5.0 (iPhone; CPU iPhone OS ".concat(this.getIosVer(), " like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1;");
-                        this.h5stTool = new h5st_pro_1.H5ST("8dd95", this.user.UserAgent, this.fp, 'https://bnzf.jd.com/?activityId=pTTvJeSTrpthgk9ASBVGsw', 'https://bnzf.jd.com', this.user.UserName);
+                        this.h5stTool = new h5st_pro_1.H5ST("8dd95", this.user.UserAgent, fp, 'https://bnzf.jd.com/?activityId=pTTvJeSTrpthgk9ASBVGsw', 'https://bnzf.jd.com', this.user.UserName);
                         return [4 /*yield*/, this.h5stTool.__genAlgo()];
                     case 3:
-                        _f.sent();
-                        _f.label = 4;
+                        _e.sent();
+                        _e.label = 4;
                     case 4:
-                        _f.trys.push([4, 17, , 18]);
+                        _e.trys.push([4, 17, , 18]);
                         return [4 /*yield*/, this.api('happyDigHome', { "linkId": "pTTvJeSTrpthgk9ASBVGsw" })];
                     case 5:
-                        res = _f.sent();
+                        res = _e.sent();
                         console.log('助力码', res.data.markedPin, res.data.inviteCode);
                         this.shareCodesSelf.push({ inviter: res.data.markedPin, inviteCode: res.data.inviteCode });
                         return [4 /*yield*/, this.api('apTaskList', { "linkId": "pTTvJeSTrpthgk9ASBVGsw" })];
                     case 6:
-                        res = _f.sent();
+                        res = _e.sent();
                         return [4 /*yield*/, this.wait(1000)];
                     case 7:
-                        _f.sent();
-                        _f.label = 8;
+                        _e.sent();
+                        _e.label = 8;
                     case 8:
-                        _f.trys.push([8, 14, 15, 16]);
-                        _c = __values(res.data), _d = _c.next();
-                        _f.label = 9;
+                        _e.trys.push([8, 14, 15, 16]);
+                        _b = __values(res.data), _c = _b.next();
+                        _e.label = 9;
                     case 9:
-                        if (!!_d.done) return [3 /*break*/, 13];
-                        t = _d.value;
+                        if (!!_c.done) return [3 /*break*/, 13];
+                        t = _c.value;
                         if (!(t.taskType === 'BROWSE_CHANNEL' && t.taskDoTimes === 0 && t.taskLimitTimes === 1)) return [3 /*break*/, 12];
                         console.log(t.taskShowTitle);
                         return [4 /*yield*/, this.api('apDoTask', { "linkId": "pTTvJeSTrpthgk9ASBVGsw", "taskType": "BROWSE_CHANNEL", "taskId": t.id, "channel": 4, "itemId": encodeURIComponent(t.taskSourceUrl), "checkVersion": false })];
                     case 10:
-                        data = _f.sent();
+                        data = _e.sent();
                         return [4 /*yield*/, this.wait(1000)];
                     case 11:
-                        _f.sent();
+                        _e.sent();
                         if (data.success) {
                             console.log('任务完成');
                         }
                         else {
                             this.o2s(data, '任务失败');
                         }
-                        _f.label = 12;
+                        _e.label = 12;
                     case 12:
-                        _d = _c.next();
+                        _c = _b.next();
                         return [3 /*break*/, 9];
                     case 13: return [3 /*break*/, 16];
                     case 14:
-                        e_1_1 = _f.sent();
+                        e_1_1 = _e.sent();
                         e_1 = { error: e_1_1 };
                         return [3 /*break*/, 16];
                     case 15:
                         try {
-                            if (_d && !_d.done && (_e = _c["return"])) _e.call(_c);
+                            if (_c && !_c.done && (_d = _b["return"])) _d.call(_b);
                         }
                         finally { if (e_1) throw e_1.error; }
                         return [7 /*endfinally*/];
                     case 16: return [3 /*break*/, 18];
                     case 17:
-                        e_2 = _f.sent();
+                        e_2 = _e.sent();
                         console.log('error', e_2);
                         return [3 /*break*/, 18];
                     case 18: return [2 /*return*/];
@@ -229,33 +228,33 @@ var Jd_speed_wabao = /** @class */ (function (_super) {
     };
     Jd_speed_wabao.prototype.help = function (users) {
         return __awaiter(this, void 0, void 0, function () {
-            var res, shareCodesHW, shareCodes, users_1, users_1_1, user, shareCodes_1, shareCodes_1_1, code, e_3_1, e_4, e_5_1;
-            var e_5, _a, e_3, _b;
-            return __generator(this, function (_c) {
-                switch (_c.label) {
+            var res, shareCodesHW, shareCodes, users_1, users_1_1, user, fp, _a, shareCodes_1, shareCodes_1_1, code, e_3_1, e_4, e_5_1;
+            var e_5, _b, e_3, _c;
+            return __generator(this, function (_d) {
+                switch (_d.label) {
                     case 0:
                         console.log('内部助力');
                         this.o2s(this.shareCodesSelf);
                         shareCodesHW = [];
-                        _c.label = 1;
+                        _d.label = 1;
                     case 1:
-                        _c.trys.push([1, 23, 24, 25]);
+                        _d.trys.push([1, 25, 26, 27]);
                         users_1 = __values(users), users_1_1 = users_1.next();
-                        _c.label = 2;
+                        _d.label = 2;
                     case 2:
-                        if (!!users_1_1.done) return [3 /*break*/, 22];
+                        if (!!users_1_1.done) return [3 /*break*/, 24];
                         user = users_1_1.value;
                         this.user = user;
                         this.user.UserAgent = "jdltapp;iPhone;3.9.2;Mozilla/5.0 (iPhone; CPU iPhone OS ".concat(this.getIosVer(), " like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1;");
                         console.log("\n\u5F00\u59CB\u3010\u4EAC\u4E1C\u8D26\u53F7".concat(user.index + 1, "\u3011").concat(user.UserName, "\n"));
-                        _c.label = 3;
+                        _d.label = 3;
                     case 3:
-                        _c.trys.push([3, 20, , 21]);
+                        _d.trys.push([3, 22, , 23]);
                         if (!(shareCodesHW.length === 0)) return [3 /*break*/, 5];
                         return [4 /*yield*/, this.getshareCodeHW('fcwb')];
                     case 4:
-                        shareCodesHW = _c.sent();
-                        _c.label = 5;
+                        shareCodesHW = _d.sent();
+                        _d.label = 5;
                     case 5:
                         if (user.index === 0) {
                             shareCodes = __spreadArray(__spreadArray([], __read(shareCodesHW), false), __read(this.shareCodesSelf), false);
@@ -263,75 +262,83 @@ var Jd_speed_wabao = /** @class */ (function (_super) {
                         else {
                             shareCodes = __spreadArray(__spreadArray([], __read(this.shareCodesSelf), false), __read(shareCodesHW), false);
                         }
-                        _c.label = 6;
+                        _a = process.env.FP_8DD95;
+                        if (_a) return [3 /*break*/, 7];
+                        return [4 /*yield*/, this.getFp()];
                     case 6:
-                        _c.trys.push([6, 17, 18, 19]);
-                        shareCodes_1 = (e_3 = void 0, __values(shareCodes)), shareCodes_1_1 = shareCodes_1.next();
-                        _c.label = 7;
+                        _a = (_d.sent());
+                        _d.label = 7;
                     case 7:
-                        if (!!shareCodes_1_1.done) return [3 /*break*/, 16];
+                        fp = _a;
+                        _d.label = 8;
+                    case 8:
+                        _d.trys.push([8, 19, 20, 21]);
+                        shareCodes_1 = (e_3 = void 0, __values(shareCodes)), shareCodes_1_1 = shareCodes_1.next();
+                        _d.label = 9;
+                    case 9:
+                        if (!!shareCodes_1_1.done) return [3 /*break*/, 18];
                         code = shareCodes_1_1.value;
                         console.log("\u8D26\u53F7".concat(user.index + 1, " ").concat(user.UserName, " \u53BB\u52A9\u529B ").concat(code.inviteCode));
-                        this.h5stTool = new h5st_pro_1.H5ST("8dd95", this.user.UserAgent, this.fp, "https://bnzf.jd.com/?activityId=pTTvJeSTrpthgk9ASBVGsw&inviterId=".concat(code.inviter, "&inviterCode=").concat(code.inviteCode, "&utm_source=iosapp&utm_medium=liteshare&utm_campaign=&utm_term=Qqfriends&ad_od=share"), 'https://bnzf.jd.com', user.UserName);
+                        this.h5stTool = new h5st_pro_1.H5ST("8dd95", this.user.UserAgent, fp, "https://bnzf.jd.com/?activityId=pTTvJeSTrpthgk9ASBVGsw&inviterId=".concat(code.inviter, "&inviterCode=").concat(code.inviteCode, "&utm_source=iosapp&utm_medium=liteshare&utm_campaign=&utm_term=Qqfriends&ad_od=share"), 'https://bnzf.jd.com', user.UserName);
                         return [4 /*yield*/, this.h5stTool.__genAlgo()];
-                    case 8:
-                        _c.sent();
+                    case 10:
+                        _d.sent();
                         return [4 /*yield*/, this.api('happyDigHelp', { "linkId": "pTTvJeSTrpthgk9ASBVGsw", "inviter": code.inviter, "inviteCode": code.inviteCode })];
-                    case 9:
-                        res = _c.sent();
-                        if (!(res.code === 0)) return [3 /*break*/, 11];
+                    case 11:
+                        res = _d.sent();
+                        if (!(res.code === 0)) return [3 /*break*/, 13];
                         console.log('助力成功');
                         return [4 /*yield*/, this.wait(4000)];
-                    case 10:
-                        _c.sent();
-                        return [3 /*break*/, 16];
-                    case 11:
-                        if (!(res.code === 16144)) return [3 /*break*/, 13];
+                    case 12:
+                        _d.sent();
+                        return [3 /*break*/, 18];
+                    case 13:
+                        if (!(res.code === 16144)) return [3 /*break*/, 15];
                         console.log('上限');
                         return [4 /*yield*/, this.wait(4000)];
-                    case 12:
-                        _c.sent();
-                        return [3 /*break*/, 16];
-                    case 13:
+                    case 14:
+                        _d.sent();
+                        return [3 /*break*/, 18];
+                    case 15:
                         console.log(res.code, res.errMsg);
                         return [4 /*yield*/, this.wait(4000)];
-                    case 14:
-                        _c.sent();
-                        _c.label = 15;
-                    case 15:
-                        shareCodes_1_1 = shareCodes_1.next();
-                        return [3 /*break*/, 7];
-                    case 16: return [3 /*break*/, 19];
+                    case 16:
+                        _d.sent();
+                        _d.label = 17;
                     case 17:
-                        e_3_1 = _c.sent();
+                        shareCodes_1_1 = shareCodes_1.next();
+                        return [3 /*break*/, 9];
+                    case 18: return [3 /*break*/, 21];
+                    case 19:
+                        e_3_1 = _d.sent();
                         e_3 = { error: e_3_1 };
-                        return [3 /*break*/, 19];
-                    case 18:
+                        return [3 /*break*/, 21];
+                    case 20:
                         try {
-                            if (shareCodes_1_1 && !shareCodes_1_1.done && (_b = shareCodes_1["return"])) _b.call(shareCodes_1);
+                            if (shareCodes_1_1 && !shareCodes_1_1.done && (_c = shareCodes_1["return"])) _c.call(shareCodes_1);
                         }
                         finally { if (e_3) throw e_3.error; }
                         return [7 /*endfinally*/];
-                    case 19: return [3 /*break*/, 21];
-                    case 20:
-                        e_4 = _c.sent();
+                    case 21: return [3 /*break*/, 23];
+                    case 22:
+                        e_4 = _d.sent();
                         console.log('error', e_4);
-                        return [3 /*break*/, 21];
-                    case 21:
+                        return [3 /*break*/, 23];
+                    case 23:
                         users_1_1 = users_1.next();
                         return [3 /*break*/, 2];
-                    case 22: return [3 /*break*/, 25];
-                    case 23:
-                        e_5_1 = _c.sent();
+                    case 24: return [3 /*break*/, 27];
+                    case 25:
+                        e_5_1 = _d.sent();
                         e_5 = { error: e_5_1 };
-                        return [3 /*break*/, 25];
-                    case 24:
+                        return [3 /*break*/, 27];
+                    case 26:
                         try {
-                            if (users_1_1 && !users_1_1.done && (_a = users_1["return"])) _a.call(users_1);
+                            if (users_1_1 && !users_1_1.done && (_b = users_1["return"])) _b.call(users_1);
                         }
                         finally { if (e_5) throw e_5.error; }
                         return [7 /*endfinally*/];
-                    case 25: return [2 /*return*/];
+                    case 27: return [2 /*return*/];
                 }
             });
         });
