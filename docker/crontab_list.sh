5c255a83a71e9f4a87e192a71d14ab468d4f40d5
ronghuaxueleng <caoqianghappy@126.com>
2022-09-24 12:38:22 +0000

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -280,6 +280,22 @@
 15,30,45 0 * * * node /scripts/jd_zjd.ts >> /scripts/logs/jd_zjb.log 2>&1
 # 平行时空
 10 6,20 * * * node /scripts/jd_pxsk.js >> /scripts/logs/jd_pxsk.log 2>&1
+# 导出所有互助码清单，日志在log/export_sharecodes下
+48 5 * * * bash export_sharecodes
+#拾取好友与自己零件、厂长翻倍任务、点击厂长任务
+0 1,18 * * * node /scripts/jd_factory_component.js >> /scripts/logs/jd_factory_component.log 2>&1
+#财富岛提现
+# 0 0,12 * * * node /scripts/jd_cfdtx.js >> /scripts/logs/jd_cfdtx.log 2>&1
+#生成财富岛提现token
+# 59 23 * * * node /scripts/get_jx_token.js >> /scripts/logs/get_jx_token.log 2>&1
+#生成京喜农场互助码
+59 23 * * * node /scripts/get_jxnc_token.js >> /scripts/logs/get_jxnc_token.log 2>&1
+#获取助力码
+58 20 * * 6 node /scripts/jd_get_share_code_4_bot.js >> /scripts/logs/jd_get_share_code_4_bot.log 2>&1
+# 财富岛热气球
+1 * * * * node /scripts/jd_cfd_loop.js >> /scripts/logs/jd_cfd_loop.log 2>&1
+# 预存验证
+50 * * * * node /scripts/jd_validate_Worker.js >> /scripts/logs/jd_validate_Worker.log 2>&1
 # 每3天的23:50分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重)
 50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecodeCollection' | xargs rm -rf
 #收集助力码
