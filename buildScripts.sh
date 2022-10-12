5c0ac80e7d73b6884ff2d79cc7aea8778902f531
ronghuaxueleng <caoqianghappy@126.com>
2022-10-12 14:07:38 +0800

    up

--- buildScripts.sh
+++ buildScripts.sh
@@ -12,7 +12,7 @@ function replaceShareCode() {
         sed -i 's/$.homeInfo?.petinfo/$.homeInfo.petinfo/g' $1.js
         sed -i 's/$.homeInfo?.coins/$.homeInfo.coins/g' $1.js
         sed -i -e '1h;2,$H;$!d;g' -re 's/let shareCodes = \[(.*\n)+?]/let shareCodes = []/g' $1.js
-        for file in `ls $1*`; do
+        for file in $(ls $1*); do
             basename=${file%%.*}
             replacename=${basename//$1/$1_$2}
             mv $file $replacename.js
@@ -25,7 +25,7 @@ function replaceShareCodeV1() {
     if [ ! -f "$1" ]; then
         sed -i 's/readShareCodeRes = await readShareCode();/readShareCodeRes = {"code": -1};/g' $1.js
         sed -i -e '1h;2,$H;$!d;g' -re 's/let shareCodes = \[(.*\n)+?]/let shareCodes = []/g' $1.js
-        for file in `ls $1*`; do
+        for file in $(ls $1*); do
             basename=${file%%.*}
             replacename=${basename//$1/$1_$2}
             mv $file $replacename.js
@@ -37,7 +37,7 @@ function replaceShareCodeV1() {
 function deleteShareCode() {
     if [ ! -f "$1" ]; then
         sed -i '/await readShareCode();/d' $1.js
-        for file in `ls $1*`; do
+        for file in $(ls $1*); do
             basename=${file%%.*}
             replacename=${basename//$1/$1_$2}
             mv $file $replacename.js
@@ -75,44 +75,44 @@ function deleteShareCode() {
 # EOF
 # sed '/^$/d' sngxprov2p/docker/crontab_list.sh
 
-git clone https://github.com/JDHelloWorld/jd_scripts.git JDHelloWorld
-[ ! -d JDHelloWorld/docker ] && mkdir -p JDHelloWorld/docker
-json=$(cat JDHelloWorld/iOS/QX.json)
-crontab_list=""
-for row in $(echo "${json}" | jq -r '.task[] | @base64'); do
-    _jq() {
-        echo ${row} | base64 --decode | jq -r ${1}
-    }
+# git clone https://github.com/JDHelloWorld/jd_scripts.git JDHelloWorld
+# [ ! -d JDHelloWorld/docker ] && mkdir -p JDHelloWorld/docker
+# json=$(cat JDHelloWorld/iOS/QX.json)
+# crontab_list=""
+# for row in $(echo "${json}" | jq -r '.task[] | @base64'); do
+#     _jq() {
+#         echo ${row} | base64 --decode | jq -r ${1}
+#     }
 
-    config=$(_jq '.config')
-    # configs=(${config//,/ })
-    crontab_list+=$(echo "${config}" | perl -pe "s|(\S+\s\S+\s\S+\s\S+\s(?:\S+\s)?)https:\/\/raw\.githubusercontent\.com\/JDHelloWorld\/jd_scripts\/main\/(\S+_?\w+)\.js(?:\s+)?,(?:\s+)?tag=(.+)(?:\s+)?,(?:\s+)?img-url=.+|\n# \3\n\1node /scripts/\2.js >> /scripts/logs/\2.log 2>&1|")
-done
-cat > JDHelloWorld/docker/crontab_list.sh <<EOF
-$(echo -e "$crontab_list")
-EOF
-sed '/^$/d' JDHelloWorld/docker/crontab_list.sh
+#     config=$(_jq '.config')
+#     # configs=(${config//,/ })
+#     crontab_list+=$(echo "${config}" | perl -pe "s|(\S+\s\S+\s\S+\s\S+\s(?:\S+\s)?)https:\/\/raw\.githubusercontent\.com\/JDHelloWorld\/jd_scripts\/main\/(\S+_?\w+)\.js(?:\s+)?,(?:\s+)?tag=(.+)(?:\s+)?,(?:\s+)?img-url=.+|\n# \3\n\1node /scripts/\2.js >> /scripts/logs/\2.log 2>&1|")
+# done
+# cat > JDHelloWorld/docker/crontab_list.sh <<EOF
+# $(echo -e "$crontab_list")
+# EOF
+# sed '/^$/d' JDHelloWorld/docker/crontab_list.sh
 
-cd JDHelloWorld
-cp -rf ${ShellDir}/replace/* ./
+# cd JDHelloWorld
+# cp -rf ${ShellDir}/replace/* ./
 
-replaceShareCode jd_dreamFactory JDHelloWorld
-replaceShareCode jd_fruit JDHelloWorld
-replaceShareCode jd_jdfactory JDHelloWorld
-replaceShareCode jd_pet JDHelloWorld
-replaceShareCode jd_plantBean JDHelloWorld
-replaceShareCode jd_sgmh JDHelloWorld
+# replaceShareCode jd_dreamFactory JDHelloWorld
+# replaceShareCode jd_fruit JDHelloWorld
+# replaceShareCode jd_jdfactory JDHelloWorld
+# replaceShareCode jd_pet JDHelloWorld
+# replaceShareCode jd_plantBean JDHelloWorld
+# replaceShareCode jd_sgmh JDHelloWorld
 
-npm install
-npm install -g npm npm-install-peers
-npm install -g ts-node typescript --unsafe-perm=true --allow-root
-npm install -g ts-node typescript @types/node date-fns axios require tslib fs
-npm install --save-dev @types/node
-ls *.ts | grep -v jd_喂猪 | grep -v jd_speed_redEnvelope | xargs tsc --downlevelIteration
+# npm install
+# npm install -g npm npm-install-peers
+# npm install -g ts-node typescript --unsafe-perm=true --allow-root
+# npm install -g ts-node typescript @types/node date-fns axios require tslib fs
+# npm install --save-dev @types/node
+# ls *.ts | grep -v jd_喂猪 | grep -v jd_speed_redEnvelope | xargs tsc --downlevelIteration
 
-rm -rf node_modules
-rm -rf package-lock.json
-cd ${ShellDir}
+# rm -rf node_modules
+# rm -rf package-lock.json
+# cd ${ShellDir}
 
 git clone -b master https://github.com/okyyds/yyds.git yyds
 cd yyds
@@ -129,33 +129,33 @@ replaceShareCode jd_sgmh yyds
 replaceShareCode jd_jxmc yyds
 cd ${ShellDir}
 
-git clone https://github.com/Akali5/jd-depot.git jddepot
-cd jddepot
-replaceShareCode jd_cash jddepot
-replaceShareCode jd_cfd jddepot
-replaceShareCode jd_dreamFactory jddepot
-replaceShareCode jd_fruit jddepot
-replaceShareCode jd_health jddepot
-replaceShareCode jd_jdfactory jddepot
-replaceShareCodeV1 jd_pet jddepot
-replaceShareCode jd_plantBean jddepot
-replaceShareCode jd_sgmh jddepot
-replaceShareCode jd_jxmc jddepot
-cd ${ShellDir}
+# git clone https://github.com/Akali5/jd-depot.git jddepot
+# cd jddepot
+# replaceShareCode jd_cash jddepot
+# replaceShareCode jd_cfd jddepot
+# replaceShareCode jd_dreamFactory jddepot
+# replaceShareCode jd_fruit jddepot
+# replaceShareCode jd_health jddepot
+# replaceShareCode jd_jdfactory jddepot
+# replaceShareCodeV1 jd_pet jddepot
+# replaceShareCode jd_plantBean jddepot
+# replaceShareCode jd_sgmh jddepot
+# replaceShareCode jd_jxmc jddepot
+# cd ${ShellDir}
 
-git clone https://github.com/ShuaiLeiLu/JD_Scripts.git ShuaiLeiLu
-cd ShuaiLeiLu
-replaceShareCode jd_cash ShuaiLeiLu
-replaceShareCode jd_cfd ShuaiLeiLu
-replaceShareCode jd_dreamFactory ShuaiLeiLu
-replaceShareCode jd_fruit ShuaiLeiLu
-replaceShareCode jd_health ShuaiLeiLu
-replaceShareCode jd_jdfactory ShuaiLeiLu
-replaceShareCodeV1 jd_pet ShuaiLeiLu
-replaceShareCode jd_plantBean ShuaiLeiLu
-replaceShareCode jd_sgmh ShuaiLeiLu
-replaceShareCode jd_jxmc ShuaiLeiLu
-cd ${ShellDir}
+# git clone https://github.com/ShuaiLeiLu/JD_Scripts.git ShuaiLeiLu
+# cd ShuaiLeiLu
+# replaceShareCode jd_cash ShuaiLeiLu
+# replaceShareCode jd_cfd ShuaiLeiLu
+# replaceShareCode jd_dreamFactory ShuaiLeiLu
+# replaceShareCode jd_fruit ShuaiLeiLu
+# replaceShareCode jd_health ShuaiLeiLu
+# replaceShareCode jd_jdfactory ShuaiLeiLu
+# replaceShareCodeV1 jd_pet ShuaiLeiLu
+# replaceShareCode jd_plantBean ShuaiLeiLu
+# replaceShareCode jd_sgmh ShuaiLeiLu
+# replaceShareCode jd_jxmc ShuaiLeiLu
+# cd ${ShellDir}
 
 git clone -b scripts https://gitee.com/getready/my_actions.git MyScript
 
@@ -164,21 +164,20 @@ ScriptsDir=${ShellDir}/jd_scripts
 DockerDir=${ScriptsDir}/docker
 [ ! -d ${DockerDir} ] && mkdir -p ${DockerDir}
 ListCronSh=${DockerDir}/crontab_list.sh
-ListCronScripts2=JDHelloWorld/docker/crontab_list.sh
-ListCronScripts3=ShuaiLeiLu/docker/crontab_list.sh
+# ListCronScripts2=JDHelloWorld/docker/crontab_list.sh
+# ListCronScripts3=ShuaiLeiLu/docker/crontab_list.sh
 ListCronScripts4=MyScript/docker/crontab_list.sh
 ListCronScripts8=yyds/docker/crontab_list.sh
 
-
-cat ${ListCronScripts2} ${ListCronScripts3} ${ListCronScripts4} ${ListCronScripts8} | tr -s [:space:] | sed '$!N; /^\(.*\)\n\1$/!P; D' > ${ListCronSh}
+cat ${ListCronScripts4} ${ListCronScripts8} | tr -s [:space:] | sed '$!N; /^\(.*\)\n\1$/!P; D' >${ListCronSh}
 
 cd ${ShellDir}
 
-jq -s 'reduce .[] as $item ({}; . * $item)' JDHelloWorld/package.json Aaron/package.json yyds/package.json > package.json
+jq -s 'reduce .[] as $item ({}; . * $item)' JDHelloWorld/package.json Aaron/package.json yyds/package.json >package.json
 
-cp -rf $(ls JDHelloWorld | grep -v docker | sed "s:^:JDHelloWorld/:" | xargs) ${ScriptsDir}
+# cp -rf $(ls JDHelloWorld | grep -v docker | sed "s:^:JDHelloWorld/:" | xargs) ${ScriptsDir}
 cp -rf $(ls yyds | grep -v docker | sed "s:^:yyds/:" | xargs) ${ScriptsDir}
 cp -rf $(ls MyScript | grep -v docker | sed "s:^:MyScript/:" | xargs) ${ScriptsDir}
-cp -rf $(ls jddepot | grep -v docker | sed "s:^:jddepot/:" | xargs) ${ScriptsDir}
-cp -rf $(ls ShuaiLeiLu | grep -v docker | sed "s:^:ShuaiLeiLu/:" | xargs) ${ScriptsDir}
+# cp -rf $(ls jddepot | grep -v docker | sed "s:^:jddepot/:" | xargs) ${ScriptsDir}
+# cp -rf $(ls ShuaiLeiLu | grep -v docker | sed "s:^:ShuaiLeiLu/:" | xargs) ${ScriptsDir}
 cp -rf package.json ${ScriptsDir}
