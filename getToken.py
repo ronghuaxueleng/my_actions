8b0179355f87d2ad0ba5785a53ec0f39b7ec928a
ronghuaxueleng <caoqianghappy@126.com>
2022-11-10 09:27:43 +0800

    京东水果

---
+++ getToken.py
@@ -0,0 +1,96 @@
+'''
+python操作青龙面板接口的封装
+'''
+import requests
+import json
+
+
+class QingLong():
+    def __init__(self, ql_config):
+        json_config = json.loads(ql_config)
+
+        self.host = f"http://{json_config['host']}"
+        self.client_id = json_config['ClientID']
+        self.client_secret = json_config['ClientSecret']
+        self.token = None
+        self.task_id = []
+
+        self.header = {
+            'Host': f'{json_config["host"]}',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36 Edg/94.0.992.38'
+        }
+
+    def run(self):
+        # 获取token，启动任务
+        if not self.get_qinglong_token():
+            print("获取token失败")
+            return False
+
+        if not self.get_jd_cookie_list():
+            print("获取任务id失败")
+            return False
+
+        return True
+
+    def get_qinglong_token(self):
+        if not self.host and not self.client_id and not self.client_secret:
+            print("参数无效")
+            return False
+
+        url = self.host + "/open/auth/token"
+        data = {
+            "client_id": self.client_id,
+            "client_secret": self.client_secret
+        }
+        response = self.request_get_method(url, data)
+        if response["code"] == 200:
+            self.token = response["data"]["token"]
+            return True
+        else:
+            print(response)
+            print("认证失败,退出")
+            return False
+
+    def get_jd_cookie_list(self):
+        '''获取任务列表，并筛选出指定的名称任务id'''
+        url = f"{self.host}/open/envs?searchValue=JD_COOKIE"
+        response = self.request_get_method(url=url)
+        if response["code"] == 200:
+            with open(".env",'w') as f: # 如果filename不存在会自动创建， 'w'表示写数据，写之前会清空文件中的原有数据！
+                f.write("JD_COOKIE='" + "&".join([cookie["value"] for cookie in response["data"]]) + "'")
+            return True
+        return False
+
+    def request_get_method(self, url, params=None):
+        '''
+        青龙的get API
+        :param url:
+        :param params:
+        :return:
+        '''
+        if self.token:
+            self.header.update({"Authorization": f"Bearer {self.token}"})
+        response = requests.get(url=url, params=params, headers=self.header, timeout=3)
+        response_json = response.json()
+        return response_json
+
+    def request_post_method(self, url, data=None):
+        '''
+        青龙的post API
+        :param url:
+        :param data:
+        :return:
+        '''
+        if self.token:
+            self.header.update({"Authorization": f"Bearer {self.token}"})
+        response = requests.post(url=url, json=data, headers=self.header, timeout=3)
+        response_json = response.json()
+        # print("request_post_method青龙响应：", response_json)
+        return response_json
+
+
+if __name__ == "__main__":
+    config = '{"host":"101.43.198.240:5680","ClientID":"SqbV-KZnOQ9q","ClientSecret":"9nG6mAcGJ8_qT99jIDQZdoy9"}'
+    QingLong(config).run()
