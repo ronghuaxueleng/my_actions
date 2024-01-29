bc358118f833534888e610715ec869877327b285
ronghuaxueleng <caoqianghappy@126.com>
2024-01-29 12:08:37 +0800

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,16 +1,10 @@
 # 导出所有互助码清单，日志在log/export_sharecodes下
 48 5 * * * bash export_sharecodes
-#拾取好友与自己零件、厂长翻倍任务、点击厂长任务
-0 1,18 * * * node /scripts/jd_factory_component.js >> /scripts/logs/jd_factory_component.log 2>&1
-#财富岛提现
-# 0 0,12 * * * node /scripts/jd_cfdtx.js >> /scripts/logs/jd_cfdtx.log 2>&1
-#生成财富岛提现token
-# 59 23 * * * node /scripts/get_jx_token.js >> /scripts/logs/get_jx_token.log 2>&1
-#生成京喜农场互助码
-59 23 * * * node /scripts/get_jxnc_token.js >> /scripts/logs/get_jxnc_token.log 2>&1
-#获取助力码
-58 20 * * 6 node /scripts/jd_get_share_code_4_bot.js >> /scripts/logs/jd_get_share_code_4_bot.log 2>&1
-# 财富岛热气球
-1 * * * * node /scripts/jd_cfd_loop.js >> /scripts/logs/jd_cfd_loop.log 2>&1
 # 预存验证
 50 * * * * node /scripts/jd_validate_Worker.js >> /scripts/logs/jd_validate_Worker.log 2>&1
+# 哩布哩布画图
+*/10 * * * * python /scripts/lib_gen.py >> /scripts/logs/lib_gen.log 2>&1
+# 哩布哩布下载
+*/6 * * * * python /scripts/download_model.py >> /scripts/logs/download_model.log 2>&1
+# 哩布哩布删除历史图片
+*/10 * * * * python /scripts/del_lib_history.py >> /scripts/logs/del_lib_history.log 2>&1
